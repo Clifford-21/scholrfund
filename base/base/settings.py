c06b677ca29cb4542f41c2072a40b437a281bd79
Clifford Silindana <clifford.silindana@gmail.com>
2023-06-02 09:06:03 +0200

    Configured database setting for AWS postgresql instance.

--- base/base/settings.py
+++ base/base/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "scholrfundLiveDB",
+        'USER': 'Clifford',
+        "PASSWORD": "scholrfund2023",
+        "HOST": "scholrfundlivedb.cfhtsxwzdihq.eu-north-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
